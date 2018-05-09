39f4d1150e81f8f350e0d77709aa3b9d8fb022e9
Julius Noack <noack@solutiondrive.de>
2018-05-09 11:21:35 +0200

    added configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,7 +15,23 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('acme_sylius_example_plugin');
+        $rootNode = $treeBuilder->root('yellow_box');
+        $rootNode
+            ->children()
+            ->variableNode('jira_token')->end()
+            ->variableNode('jira_url')->end()
+            ->variableNode('jira_api_url')->end()
+            ->variableNode('jira_token_request_url')->end()
+            ->variableNode('jira_token_auth_url')->end()
+            ->variableNode('jira_token_access_url')->end()
+            ->variableNode('jira_private_key')->end()
+            ->variableNode('jira_private_passphrase')->end()
+            ->variableNode('jira_projekt_key')->end()
+            ->variableNode('jira_displayed_issue_status')->end()
+            ->variableNode('jira_transition_approve')->end()
+            ->variableNode('jira_transition_decline')->end()
+            ->variableNode('jira_decline_reason_field')->end()
+            ->end();
 
         return $treeBuilder;
     }
